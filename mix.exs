b04423818e459b67cc53601d8e79f37447265b1a
Mischov <mmischov@gmail.com>
2017-04-03 14:29:58 -0500

    Prepare mix.exs for release

--- mix.exs
+++ mix.exs
@@ -1,22 +1,26 @@
 defmodule Html5ever.Mixfile do
   use Mix.Project
 
+  @version "0.4.0-rc"
+
   def project do
     [app: :meeseeks_html5ever,
-     version: "0.3.0",
+     name: "MeeseeksHtml5ever",
+     version: @version,
+     description: description(),
      elixir: "~> 1.4",
+     deps: deps(),
+     package: package(),
+     source_url: "https://github.com/mischov/meeseeks_html5ever",
+     docs: [main: "MeeseeksHtml5ever"],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      compilers: [:rustler] ++ Mix.compilers(),
-     rustler_crates: rustler_crates(),
-     deps: deps(),
-     description: description(),
-     package: package()]
+     rustler_crates: rustler_crates()]
   end
 
   def rustler_crates do
-    [
-      meeseeks_html5ever_nif: [
+    [meeseeks_html5ever_nif: [
         path: "native/meeseeks_html5ever_nif",
         cargo: :system,
         default_features: false,
@@ -27,41 +31,30 @@ defmodule Html5ever.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:rustler, "~> 0.9"},
-     {:ex_doc, ">= 0.0.0", only: :dev}]
+
+     # docs
+     {:ex_doc, "~> 0.14", only: :docs},
+     {:markdown, github: "devinus/markdown", only: :docs}]
   end
 
   defp description do
     """
-    Meeseeks specific NIF binding of html5ever using rustler.
+    Meeseeks-specific NIF binding of html5ever using Rustler.
     """
   end
 
   defp package do
-    [
-      files: ["lib", "native", "mix.exs", "README.md"],
-      maintainers: ["Mischov"],
-      licenses: ["MIT", "Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/mischov/meeseeks_html5ever"},
-    ]
+    [maintainers: ["Mischov"],
+     licenses: ["MIT", "Apache-2.0"],
+     files: ["lib", "native", "mix.exs", "README.md", "LICENSE-MIT", "LICENSE-APACHE"],
+     links: %{"GitHub" => "https://github.com/mischov/meeseeks_html5ever",
+              "Docs" => "https://hexdocs.pm/meeseeks_html5ever"}]
   end
 
 end
