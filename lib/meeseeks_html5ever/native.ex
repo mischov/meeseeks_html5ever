5f8387a7f35358bdf0cf670c89bb0a69fd617f77
Mischov <mmischov@gmail.com>
2017-04-08 14:28:35 -0500

    Move NifNotLoadedError inside of MeeseeksHtml5ever.Native

--- lib/meeseeks_html5ever/native.ex
+++ lib/meeseeks_html5ever/native.ex
@@ -1,13 +1,13 @@
-defmodule NifNotLoadedError do
-  @moduledoc false
-
-  defexception message: "nif not loaded"
-end
-
 defmodule MeeseeksHtml5ever.Native do
   @moduledoc false
 
   use Rustler, otp_app: :meeseeks_html5ever, crate: "meeseeks_html5ever_nif"
+
+  defmodule NifNotLoadedError do
+    @moduledoc false
+
+    defexception message: "nif not loaded"
+  end
 
   def parse_async(_binary), do: err()
   def parse_sync(_binary), do: err()
