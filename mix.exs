762146ac1e84bda15c68bb147d937c79fd959c59
Mischov <mmischov@gmail.com>
2019-02-23 12:57:59 -0600

    Remove support for Elixir 1.3

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule MeeseeksHtml5ever.Mixfile do
       name: "MeeseeksHtml5ever",
       version: @version,
       description: description(),
-      elixir: "~> 1.3",
+      elixir: "~> 1.4",
       deps: deps(),
       package: package(),
       source_url: "https://github.com/mischov/meeseeks_html5ever",
