24540b720ef0aa06aaaa58ebd3ffc2586ae0083b
Mischov <mmischov@gmail.com>
2023-03-26 00:58:18 -0500

    Move Rustler config into native.ex

--- lib/meeseeks_html5ever/native.ex
+++ lib/meeseeks_html5ever/native.ex
@@ -1,19 +1,10 @@
 defmodule MeeseeksHtml5ever.Native do
   @moduledoc false
 
-  use Rustler, otp_app: :meeseeks_html5ever, crate: :meeseeks_html5ever_nif
-
-  defmodule NifNotLoadedError do
-    @moduledoc false
-
-    defexception message: "nif not loaded"
-  end
+  use Rustler, otp_app: :meeseeks_html5ever, crate: :meeseeks_html5ever_nif, mode: :release
 
   def parse_html(_binary), do: err()
-
   def parse_xml(_binary), do: err()
 
-  defp err() do
-    :erlang.nif_error(%NifNotLoadedError{})
-  end
+  defp err(), do: :erlang.nif_error(:nif_not_loaded)
 end
